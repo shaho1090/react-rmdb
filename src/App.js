0ccd078fd908a60023a98a2eeac5cc6e27cfaf7b
shaho1090 <shaho.parvini@gmail.com>
2021-08-23 12:22:09 +0430

    import and use components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+//components
+import Header from './components/Header';
+import Home from './components/Home';
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+
 function App() {
-  return (
+  return ( 
     <div className="App">
-      Start here.
+      <Header/>
+         <Home/>  
+      <GlobalStyle />
     </div>
   );
 }
